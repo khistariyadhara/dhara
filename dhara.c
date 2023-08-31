84b14ed0837fa29692615bd99bae3a76a6ac74bd
dharakhistariya <khistariyadhara@gmail.com>
2023-08-31 19:07:59 +0530

    this is my first repo

---
+++ dhara.c
@@ -0,0 +1,341 @@
+[6:23 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+    printf("    *\n");
+    printf("   * *\n");
+   printf("  *   *\n");
+  printf(" * * * * \n");
+ printf("*       *\n");
+  return 0;
+}
+[6:32 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("  * * **\n");
+  printf("  *     * \n");
+  printf("  *     *\n");
+  printf("  * * * * \n");
+  printf("  *     *\n");
+  printf("  *     *\n");
+  printf("  * * ** \n");
+  return 0;
+}
+[6:33 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("    * * **\n");
+  printf("  *      \n");
+  printf("  *     \n");
+  printf("  *  \n");
+  printf("  *     \n");
+  printf("  *     \n");
+  printf("    * * ** \n");
+  return 0;
+}
+[6:35 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("  * * **\n");
+  printf("  *     * \n");
+  printf("  *     *\n");
+  printf("  *     * \n");
+  printf("  *     *\n");
+  printf("  *     *\n");
+  printf("  * * ** \n");
+  return 0;
+}
+[6:36 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("  * * * * \n");
+  printf("  *       \n");
+  printf("  *       \n");
+  printf("  * * * *       \n");
+  printf("  *       \n");
+  printf("  *       \n");
+  printf("  * * * * \n");
+  return 0;
+}
+[6:36 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("  * * * * \n");
+  printf("  *       \n");
+  printf("  *       \n");
+  printf("  * * * *       \n");
+  printf("  *       \n");
+  printf("  *       \n");
+  printf("  *  \n");
+  return 0;
+}
+[6:37 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("  * * * * \n");
+  printf("  *       \n");
+  printf("  *       \n");
+  printf("  * * * *       \n");
+  printf("  *      * \n");
+  printf("  *      * \n");
+  printf("  * * * * \n");
+  return 0;
+}
+[6:38 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("  *      * \n");
+  printf("  *      * \n");
+  printf("  *      * \n");
+  printf("  * * *  *      \n");
+  printf("  *      * \n");
+  printf("  *      * \n");
+  printf("  *      * \n");
+  return 0;
+}
+[6:40 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("   * * * * *    \n");
+  printf("      *      \n");
+  printf("      *       \n");
+  printf("      *      \n");
+  printf("      *       \n");
+  printf("      *       \n");
+  printf("   * * * * * \n");
+  return 0;
+}
+[6:41 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("   * * * * *    \n");
+  printf("       *      \n");
+  printf("       *       \n");
+  printf("       *      \n");
+  printf("       *       \n");
+  printf("  *    *       \n");
+  printf("   * *  \n");
+  return 0;
+}
+[6:42 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("  *      *    \n");
+  printf("  *    *     \n");
+  printf("  *  *       \n");
+  printf("  *       \n");
+  printf("  *  *     \n");
+  printf("  *    *    \n");
+  printf("  *      *\n");
+  return 0;
+}
+[6:43 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("  *          \n");
+  printf("  *         \n");
+  printf("  *         \n");
+  printf("  *       \n");
+  printf("  *       \n");
+  printf("  *        \n");
+  printf("  *  * * * *    \n");
+  return 0;
+}
+[6:44 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("  *       *    \n");
+  printf("  * *   * *   \n");
+  printf("  *   *   *    \n");
+  printf("  *       *  \n");
+  printf("  *       * \n");
+  printf("  *       *   \n");
+  printf("  *       *  \n");
+  return 0;
+}
+[6:46 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("  *        *    \n");
+  printf("  * *      *   \n");
+  printf("  *   *    *    \n");
+  printf("  *     *  *  \n");
+  printf("  *      * * \n");
+  printf("  *        *   \n");
+ 
+  return 0;
+}
+[6:48 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("   **\n");
+  printf("  *     * \n");
+  printf("  *     *\n");
+  printf("  *     * \n");
+  printf("  *     *\n");
+  printf("  *     *\n");
+  printf("   ** \n");
+  return 0;
+}
+[6:50 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("  * * * * \n");
+  printf("  *      * \n");
+  printf("  *     *    \n");
+  printf("  * * *        \n");
+  printf("  *       \n");
+  printf("  *       \n");
+  printf("  *  \n");
+  return 0;
+}
+[6:52 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("    * *\n");
+  printf("  *     * \n");
+  printf("  *     *\n");
+  printf("  *     * \n");
+  printf("  *     *\n");
+  printf("  *     *\n");
+  printf("    * *  \n");
+  return 0;
+}
+[6:54 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("    * *\n");
+  printf("  *     * \n");
+  printf("  *     *\n");
+  printf("  *     * \n");
+  printf("  *     *\n");
+  printf("  *     *\n");
+  printf("    * * \n");
+  printf("       *       ");
+  return 0;
+}
+[6:55 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("  * * * * \n");
+  printf("  *      * \n");
+  printf("  *     *    \n");
+  printf("  * * *        \n");
+  printf("  *    *      \n");
+  printf("  *      *    \n");
+  printf("  *        *\n");
+  return 0;
+}
+[6:57 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("   * * * \n");
+  printf("  *        \n");
+  printf("    *       \n");
+  printf("      *        \n");
+  printf("        *      \n");
+  printf("       *      \n");
+  printf(" * * *\n");
+  return 0;
+}
+[6:58 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("   * * * * *    \n");
+  printf("      *      \n");
+  printf("      *       \n");
+  printf("      *      \n");
+  printf("      *       \n");
+  printf("      *       \n");
+  printf("      *\n");
+  return 0;
+}
+[7:00 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("      *      *\n");
+  printf("      *      *\n");
+  printf("      *      *\n");
+  printf("      *      *\n");
+  printf("      *      *\n");
+  printf("      *      * \n");
+  printf("        * *   \n");
+  return 0;
+}
+[7:01 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("      *          *\n");
+  printf("       *        *\n");
+  printf("        *      *\n");
+  printf("          *  *   \n");
+  printf("           *  \n");
+  return 0;
+}
+[7:03 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("      *          *         *      \n");
+  printf("       *        *  *      *         \n");
+  printf("        *      *    *    *       \n");
+  printf("          *  *       *  *          \n");
+  printf("           *          *          \n");
+  return 0;
+}
+[7:04 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("  *    *\n");
+  printf("   *  *      \n");
+  printf("    *           \n");
+  printf("   *  *      \n");
+  printf("  *    *  \n");
+  return 0;
+}
+[7:06 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("  *    *\n");
+  printf("   *  *      \n");
+  printf("    *           \n");
+  printf("   *        \n");
+  printf("  *      \n");
+  return 0;
+}
+[7:07 pm, 29/08/2023] Dhara 🌸: #include <stdio.h>
+
+int main ()
+{
+  printf("  * * * * * *\n");
+  printf("          * \n");
+  printf("        *      \n");
+  printf("      *     \n");
+  printf("    *   \n");
+  printf("  * * * * * *\n");
+  return 0;
+}
